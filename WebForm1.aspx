9fb52877387df239b41f86b1d5f55b5bf2bc8697
黑翼 <lucky4916@hotmail.com>
2020-12-30 10:04:45 +0800

    加入專案檔案。

---
+++ WebForm1.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="TestWebCrawler.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" >
+                <Columns>
+                    <asp:ImageField DataImageUrlField="ItemPicture" HeaderText="圖片">
+                        <ControlStyle Height="100px" Width="100px" />
+                    </asp:ImageField>
+                    <asp:BoundField DataField="ItemName" HeaderText="商品名稱" />
+                    <asp:BoundField DataField="ItemFacts" HeaderText="商品介紹" />
+                    <asp:BoundField DataField="ItemPrice" HeaderText="商品價格" />
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Net.Http;
+using AngleSharp;
+using AngleSharp.Dom;
+
+
+namespace TestWebCrawler
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        String orgUrl = "http://www.3375.com.tw";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String Url = "http://www.3375.com.tw/Menu/FoodMenu";
+           
+
+            IDocument doc = null;
+            if (GetHttpDocument(Url, ref doc))
+            {
+                PostGridView(doc);
+            }
+        }
+
+        public Boolean GetHttpDocument(String Url,ref IDocument doc)
+        {
+            HttpClient httpClient = new HttpClient();
+
+            //發送請求並取得回應內容
+            var responMessage = httpClient.GetAsync(Url).Result;
+
+            //檢查回應的伺服器狀態StatusCode是否是200 OK
+            if (responMessage.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                //讀取Content內容
+                string responseResult = responMessage.Content.ReadAsStringAsync().Result;
+                //Console.WriteLine(responResult);
+
+                //使用AngleSharp時的前置設定
+                var config = Configuration.Default;
+                var context = BrowsingContext.New(config);
+
+                //傳入response資料
+                //將我們用httpclient拿到的資料放入res.Content中()
+                doc = context.OpenAsync(res => res.Content(responseResult)).Result;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public void PostGridView(IDocument doc) 
+        {
+            DataTable FoodList = new DataTable();
+            FoodList.Columns.Add("ItemName");
+            FoodList.Columns.Add("ItemFacts");
+            FoodList.Columns.Add("ItemPrice");
+            FoodList.Columns.Add("ItemPicture");
+
+            var ItemName = doc.QuerySelectorAll("div.pro_text-wrap > h3 ");
+            var ItemFacts = doc.QuerySelectorAll("div.pro_text-wrap > p");
+            var ItemPrice = doc.QuerySelectorAll("sub.prize");
+            var ItemPicture = doc.QuerySelectorAll("div.pro-img");
+
+            for (int i = 0; i < ItemName.Length; i++)
+            {
+                //var Name = ItemName[i].TextContent.Replace("\n","");
+                var Name = ItemName[i].TextContent.Replace(ItemName[i].GetElementsByClassName("prize")[0].TextContent, "");
+                var Facts = ItemFacts[i].TextContent.Replace("\n", "");
+                var Price = ItemPrice[i].TextContent.Replace("\n", "");
+                var UrlPicture = ItemPicture[i].GetElementsByClassName("img-responsive")[0].GetAttribute("src");
+
+                DataRow NewData = FoodList.NewRow();
+                NewData["ItemName"] = Name;
+                NewData["ItemFacts"] = Facts;
+                NewData["ItemPrice"] = Price;
+                NewData["ItemPicture"] = orgUrl + UrlPicture.Trim();
+                FoodList.Rows.Add(NewData);
+            }
+
+            GridView1.DataSource = FoodList;
+            GridView1.DataBind();
+            
+        }
+
+
+
+    }
+
+}
